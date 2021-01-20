0c4f223bcc8bc07efc977171dbe7ff4a259c2980
Hoang Phong Vu TRANG <tranghoangphongvu@gmail.com>
2021-01-20 11:10:44 -0800

    finish automatisation imagetarget importation

---
+++ nutrition-reccomendation/Assets/LoadObjects.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using Vuforia;
+using System.Linq;
+
+public class LoadObjects : MonoBehaviour {
+    public GameObject Model;
+
+    void Start() {
+        // Add Call back to check when Vuforia is ready
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnStarted);
+    }
+
+    void Update() {
+
+    }
+
+    void OnStarted() {
+
+        // Set working directory to access to dataset
+        // Site : https://docs.unity3d.com/Manual/StreamingAssets.html
+        string file = "Vuforia/VuforiaMars_Images.xml";
+        string path = Application.streamingAssetsPath + "/" + file;
+
+        if(LoadDataSet(path, VuforiaUnity.StorageType.STORAGE_ABSOLUTE))
+            Debug.Log(file + " loaded");
+        else 
+            Debug.Log(file + " load failed");
+    }
+
+    private bool LoadDataSet(string filepath, VuforiaUnity.StorageType storageType) {
+        // References :
+        //  DataSet :
+        //      https://library.vuforia.com/sites/default/files/references/unity/classVuforia_1_1DataSet.html
+        ObjectTracker objTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+
+        // Deactivate current active datasets to avoid duplication
+        objTracker.Stop();
+        System.Collections.Generic.IEnumerable<DataSet> dataSetList = 
+            objTracker.GetActiveDataSets();
+        foreach(DataSet ds in dataSetList.ToList()) {
+            objTracker.DeactivateDataSet(ds);
+        }
+
+        // Check if file exists 
+        if (!DataSet.Exists(filepath, storageType)) {
+            Debug.LogError("Dataset not found!");
+            return false;
+        }
+
+        // Load DataSet 
+        DataSet dataSet = objTracker.CreateDataSet();
+        if (!dataSet.Load(filepath, storageType)) {
+            Debug.LogError("Couldn't load dataset!");
+            return false;
+        }
+
+        // Load and add to TrackableBehaviours
+        objTracker.ActivateDataSet(dataSet);
+        objTracker.Start(); 
+
+        AddMenuToImageTarget(dataSet);
+
+        return true;
+    }
+
+    private void AddMenuToImageTarget(DataSet dataSet) {
+        // Get All current TrackableBehaviour
+        System.Collections.Generic.IEnumerable<TrackableBehaviour> trackableBehaviours = 
+            TrackerManager.Instance.GetStateManager().GetTrackableBehaviours();
+
+        foreach (TrackableBehaviour trackable in trackableBehaviours) {
+            // Check if the trackable is in dataset
+            if (dataSet.Contains(trackable.Trackable)) {
+                // Get gameobject of the image target
+                GameObject go = trackable.gameObject; 
+                go.name = "Imported<" + trackable.TrackableName + ">";
+                // Add eventhandler to the Trackable for default features of detecting object
+                go.AddComponent<DefaultTrackableEventHandler>();
+                // Add TrackingHandler.cs to the Component
+                go.AddComponent<TrackingHandler>();
+                // Clone Model and add Model to ImageTarget
+                GameObject model = Instantiate(Model) as GameObject;
+                model.name = go.name + "." + model.name;
+                go.GetComponent<TrackingHandler>().Model = model; 
+            }
+        }
+    }
+
+}
---
+++ nutrition-reccomendation/Assets/TrackingHandler.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class TrackingHandler : MonoBehaviour
+{
+    public GameObject Model;
+
+    private TrackableBehaviour mTrackableBehaviour;
+    private TrackableBehaviour.Status m_PreviousStatus;
+    private TrackableBehaviour.Status m_NewStatus;
+    protected bool m_CallbackReceivedOnce = false;
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour) {
+            mTrackableBehaviour.RegisterOnTrackableStatusChanged(OnTrackableStatusChanged);
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTrackableStatusChanged(TrackableBehaviour.StatusChangeResult statusChangeResult) {
+        m_PreviousStatus = statusChangeResult.PreviousStatus;
+        m_NewStatus = statusChangeResult.NewStatus;
+
+        HandleTrackableStatusChanged();
+    }
+    
+    protected virtual void HandleTrackableStatusChanged()
+    {
+        if (!ShouldBeRendered(m_PreviousStatus) &&
+            ShouldBeRendered(m_NewStatus))
+        {
+            OnTrackingFound();
+        }
+        else if (ShouldBeRendered(m_PreviousStatus) &&
+                 !ShouldBeRendered(m_NewStatus))
+        {
+            OnTrackingLost();
+        }
+        else
+        {
+            if (!m_CallbackReceivedOnce && !ShouldBeRendered(m_NewStatus))
+            {
+                // This is the first time we are receiving this callback, and the target is not visible yet.
+                // --> Hide the augmentation.
+                OnTrackingLost();
+            }
+        }
+
+        m_CallbackReceivedOnce = true;
+    }
+
+    protected bool ShouldBeRendered(TrackableBehaviour.Status status)
+    {
+        if (status == TrackableBehaviour.Status.DETECTED ||
+            status == TrackableBehaviour.Status.TRACKED)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected virtual void OnTrackingFound() {
+        if (mTrackableBehaviour && Model) {
+            PlaceModelOnImageTarget();
+            Model.SetActive(true);
+        }
+    }
+    protected virtual void OnTrackingLost() {
+        if (mTrackableBehaviour && Model) {
+            Model.SetActive(false);
+        }
+    }
+
+    private void PlaceModelOnImageTarget() {
+            Model.transform.parent = mTrackableBehaviour.transform; // Attach Model to trackable
+            Model.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            Model.transform.localPosition = new Vector3(0.0f, 0.05f, 0.0f);
+            Model.transform.localRotation = Quaternion.identity;
+    }
+}
